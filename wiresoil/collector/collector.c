01dc28ab7dbdde4bd63ecf976917ccc27419d870
John Tims <missionpilot@gmail.com>
2010-09-27 01:42:13 +0000

    Added project files for the Collector and Sensor
    git-svn-id: svn+ssh://svn.ece.msstate.edu/data/www/htdocs/courses/design/2010/wiresoil/svn@4 db668ce9-9f08-4dc0-9949-e28e8fe2f565

---
+++ wiresoil/collector/collector.c
@@ -0,0 +1,22 @@
+#include "pic24_all.h"
+#include "vdip.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    //__builtin_write_OSCCONL(OSCCON | 0x02);
+    configBasic(HELLO_MSG);
+
+	//config SPI for VDIP1
+	VDIP_Init();
+
+	//syncs VDIP with PIC
+	VDIP_Sync();
+
+	// put vdip in short command mode
+	VDIP_SCS();
+	puts("FERTIG.\n");
+	while(1){}
+	return 0;
+}
---
+++ wiresoil/sensor/sensor.c
@@ -0,0 +1,13 @@
+#include "pic24_all.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    //__builtin_write_OSCCONL(OSCCON | 0x02);
+    configBasic(HELLO_MSG);
+
+	puts("FERTIG.\n");
+	while(1){}
+	return 0;
+}
