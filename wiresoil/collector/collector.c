9584d51d2789ec96e33615c9d1260b72512a2c87
Andrew Turner <at207@msstate.edu>
2010-10-24 22:13:03 +0000

    Timeouts and canLead/sleep time switches working
    git-svn-id: svn+ssh://svn.ece.msstate.edu/data/www/htdocs/courses/design/2010/wiresoil/svn@30 db668ce9-9f08-4dc0-9949-e28e8fe2f565

--- wiresoil/collector/collector.c
+++ wiresoil/collector/collector.c
@@ -2,14 +2,85 @@
 #include "packet.h"
 #include "snsl.h"
 
-uint8 u8_pollingTimeout;
+/****************************GLOBAL VARIABLES*********************/
+
 uint8 u8_failureLimit;
+uint8 u8_stopPolling;
+uint8 u8_numHops;
+uint32 u32_hopTimeout;
+
+union32 timer_max_val;
+
+/****************************PIN CONFIGURATION********************/
+
+#define SLEEP_INPUT _RB14
+#define TEST_SWITCH _RB8
+#define VDIP_POWER	_RB7
+#define SLEEP_TIME	_RA2
+
+/// Sleep Input pin configuration
+inline void CONFIG_SLEEP_INPUT()  {
+  CONFIG_RB14_AS_DIG_INPUT();     //use RB14 for mode input
+  DISABLE_RB14_PULLUP();
+  DELAY_US(1);
+}
+
+//Test Mode Switch pin configuration
+inline void CONFIG_TEST_SWITCH() {
+	CONFIG_RB8_AS_DIG_INPUT();
+	ENABLE_RB8_PULLUP();
+	DELAY_US(1);
+}
+
+//VDIP power pin configuration
+inline void CONFIG_VDIP_POWER() {
+	CONFIG_RB7_AS_DIG_OD_OUTPUT();
+	DELAY_US(1);
+}
+
+//Sleep time pin configuration
+inline void CONFIG_SLEEP_TIME() {
+	CONFIG_RA2_AS_DIG_INPUT();
+	ENABLE_RA2_PULLUP();
+	DELAY_US(1);
+}
+
+/****************************TIMER CONFIGURATION******************/
+
+void configTimer23(void) {
+	T2CON = T2_OFF | T2_IDLE_CON | T2_GATE_OFF | T2_32BIT_MODE_ON
+			| T2_SOURCE_INT |T2_PS_1_1;
+}
+
+void startTimer23(void) {
+	timer_max_val.u32 = usToU32Ticks(((u32_hopTimeout*u8_numHops)*1000), getTimerPrescale(T2CONbits)) - 1;
+
+	PR2 = timer_max_val.u16.ls16;	//LSW
+	PR3 = timer_max_val.u16.ms16;	//MSW
+
+	TMR3HLD = 0;	//clear timer3 value
+	TMR2 = 0;	//clear timer2 value
+	_T3IF = 0;	//clear interrupt flag
+	_T3IP = 3;	//set timer interrupt priority (must be higher than sleep interrupt)
+	_T3IE = 1;	//enable timer interrupt
+	T2CONbits.TON = 1;	//start the timer
+}
+
+/****************************POLLING FUNCTIONS******************/
 
 uint8 blocking_inChar()
 {
+	startTimer23();
     // Wait for character
-    while(!isCharReady2()){}
-    return inChar2();
+    while(!isCharReady2() && !u8_stopPolling){
+	}
+	if (!u8_stopPolling) {
+		T2CONbits.TON = 0;
+		return inChar2();
+	}
+	else {
+		return '~';
+	}
 }
 
 uint8 isMeshUp(void) {
@@ -42,6 +113,8 @@ void sendStayAwake(void) {
 //packet format: 0x1E + packet length + address + 0x03 + message
 uint8 doPoll(char c_ad1, char c_ad2, char c_ad3) {
 
+	u8_stopPolling = 0;
+
 	outChar('P');
 	outChar(':');
 	outChar(c_ad1);
@@ -72,72 +145,92 @@ uint8 doPoll(char c_ad1, char c_ad2, char c_ad3) {
 	uint8 tmp = 0;
 	uint8 packet_length = 0;
 	uint8 remaining_polls = 0;
+	uint8 u8_char;
+	
 	for(tmp = 0; tmp < 7; tmp++)
 	{
-		if (tmp == 4) packet_length = blocking_inChar() - 2;
-		else if (tmp == 6) remaining_polls = blocking_inChar();
-		else blocking_inChar();
+		u8_char = blocking_inChar();
+		if (u8_char == '~') {
+			u8_stopPolling = 1;
+			break;
+		}
+		else {
+			if (tmp == 4) packet_length = u8_char - 2;
+			else if (tmp == 6) remaining_polls = u8_char;
+		}
 	}
 
 	for(tmp = 0; tmp < packet_length; tmp++) {
-		poll_data[tmp] = blocking_inChar();
+		u8_char = blocking_inChar();
+		if (u8_char == '~') {
+			u8_stopPolling = 1;
+			break;
+		}
+		else poll_data[tmp] = u8_char;
+			//poll_data[tmp] = blocking_inChar();
 	}
+
+	if (!u8_stopPolling) {
+		
+		poll_data[packet_length] = '\0';
+		
+		outString("PACKET:`");
+		tmp = 0;
+		for(tmp = 0; tmp < 39; tmp++)
+		{
+			outUint8(poll_data[tmp]);
+			outChar(' ');
+		}
+		outString("`\n");
+		for(tmp = 0; tmp < 39; tmp++)
+		{
+			outChar(poll_data[tmp]);
+		}
 	
-	poll_data[packet_length] = '\0';
+		//uint8 *pui_node_name = &poll_data[26];
+		uint8 psz_node_name[MAX_NODE_NAME_LEN];
+		for(i = 26; i < packet_length; ++i)
+		{
+			psz_node_name[i-26] = poll_data[i];
+		}
+		psz_node_name[i-26] = 0x0;
+		
 	
-	outString("PACKET:`");
-	tmp = 0;
-	for(tmp = 0; tmp < 39; tmp++)
-	{
-		outUint8(poll_data[tmp]);
-		outChar(' ');
-	}
-	outString("`\n");
-	for(tmp = 0; tmp < 39; tmp++)
-	{
-		outChar(poll_data[tmp]);
-	}
-
-	//uint8 *pui_node_name = &poll_data[26];
-	uint8 psz_node_name[MAX_NODE_NAME_LEN];
-	for(i = 26; i < packet_length; ++i)
-	{
-		psz_node_name[i-26] = poll_data[i];
-	}
-	psz_node_name[i-26] = 0x0;
+	    uint8 psz_fmt[] = "%02X%02X%02X,"                   // node address
+	                      /*"%c%c%c%c%c%c%c%c%c%c%c%c,"   	// node name*/
+						  "%s,"
+	                      "%02x/%02x/%02x %02x:%02x:%02x,"  // timestamp [MM/DD/YY HH:MM:SS]
+	                      "%c%c%c%c%c%c%c%c%c%c,"           // temp samples
+	                      "%c%c%c%c%c%c%c%c,"               // redox samples
+	                      "%c%c\n";                         // ref voltage
 	
-
-    uint8 psz_fmt[] = "%02X%02X%02X,"                   // node address
-                      /*"%c%c%c%c%c%c%c%c%c%c%c%c,"   	// node name*/
-					  "%s,"
-                      "%02x/%02x/%02x %02x:%02x:%02x,"  // timestamp [MM/DD/YY HH:MM:SS]
-                      "%c%c%c%c%c%c%c%c%c%c,"           // temp samples
-                      "%c%c%c%c%c%c%c%c,"               // redox samples
-                      "%c%c\n";                         // ref voltage
-
-    uint8* p = poll_data;
-    uint8 psz_out[70];
-    sprintf(psz_out, psz_fmt,
-        c_ad1, c_ad2, c_ad3,
-        /*p[26],p[27],p[28],p[29],p[30],p[31],p[32],p[33],p[34],p[35],p[36],p[37],*/
-		psz_node_name,
-        p[0],p[1],p[2],p[3],p[4],p[5],
-        p[6],p[7],p[8],p[9],p[10],p[11],p[12],p[13],p[14],p[15],
-        p[16],p[17],p[18],p[19],p[20],p[21],p[22],p[23],
-        p[24],p[25]);
-
-	psz_out[69] = 0x0;
-
-	outString(psz_out);
-
-	VDIP_WriteFile("DATA.TXT", psz_out);
-
-	outChar('\n');
-
-	if (remaining_polls != 0) {
-		doPoll(c_ad1, c_ad2, c_ad3);
+	    uint8* p = poll_data;
+	    uint8 psz_out[70];
+	    sprintf(psz_out, psz_fmt,
+	        c_ad1, c_ad2, c_ad3,
+	        /*p[26],p[27],p[28],p[29],p[30],p[31],p[32],p[33],p[34],p[35],p[36],p[37],*/
+			psz_node_name,
+	        p[0],p[1],p[2],p[3],p[4],p[5],
+	        p[6],p[7],p[8],p[9],p[10],p[11],p[12],p[13],p[14],p[15],
+	        p[16],p[17],p[18],p[19],p[20],p[21],p[22],p[23],
+	        p[24],p[25]);
+	
+		psz_out[69] = 0x0;
+	
+		outString(psz_out);
+	
+		VDIP_WriteFile("DATA.TXT", psz_out);
+	
+		outChar('\n');
+	
+		if (remaining_polls != 0) {
+			doPoll(c_ad1, c_ad2, c_ad3);
+		}
 	}
-
+	else if (!SLEEP_TIME) {
+		//record node response failure
+		//do not record failure if in mesh setup mode (SLEEP_PIN == HIGH)
+	}
 	return 0x01;
 }
 
@@ -153,29 +246,7 @@ void sendEndPoll(void) {
 	outString2(sz_data);
 }
 
-#define SLEEP_INPUT _RB14
-#define TEST_SWITCH _RB8
-#define VDIP_POWER	_RB7
-
-/// Sleep Input pin configuration
-inline void CONFIG_SLEEP_INPUT()  {
-  CONFIG_RB14_AS_DIG_INPUT();     //use RB14 for mode input
-  DISABLE_RB14_PULLUP();
-  DELAY_US(1);
-}
-
-//Test Mode Switch pin configuration
-inline void CONFIG_TEST_SWITCH() {
-	CONFIG_RB8_AS_DIG_INPUT();
-	ENABLE_RB8_PULLUP();
-	DELAY_US(1);
-}
-
-//VDIP power pin configuration
-inline void CONFIG_VDIP_POWER() {
-	CONFIG_RB7_AS_DIG_OD_OUTPUT();
-	DELAY_US(1);
-}
+/****************************INTERRUPT HANDLERS*******************/
 
 void _ISRFAST _INT1Interrupt(void) {
 	uint32 u32_index = 0;
@@ -208,7 +279,16 @@ void _ISRFAST _INT1Interrupt(void) {
 	_LATB7 = 1;					//cut power to VDIP
 
 	_INT1IF = 0;
-}	
+}
+
+void _ISRFAST _T3Interrupt (void) {
+	u8_stopPolling = 1;
+	T2CONbits.TON = 0;	//stop the timer
+	outString("timer interrupt");
+	_T3IF = 0;	//clear interrupt flag
+}
+
+/****************************MAIN******************/
 
 int main(void) {
 	configClock();
@@ -218,10 +298,15 @@ int main(void) {
 	CONFIG_SLEEP_INPUT();
 	CONFIG_TEST_SWITCH();
 	CONFIG_VDIP_POWER();
+	CONFIG_SLEEP_TIME();
 
 	CONFIG_INT1_TO_RP(14);
 
-	u8_pollingTimeout = 5;
+	u8_stopPolling = 0;
+	configTimer23();
+
+	u8_numHops = 5;
+	u32_hopTimeout = 1000;
 	u8_failureLimit = 5;
 
 	if (!TEST_SWITCH) {
@@ -231,9 +316,10 @@ int main(void) {
 		outString("Setup Mode:\n\n");
 		outString("Choose an option -\n\n");
 		outString("1. Configure clock\n");
-		outString("2. Set polling timeout\n");
-		outString("3. Set node failure limit\n");
-		outString("4. Reset node ignore list\n");
+		outString("2. Set maximum number of network hops\n");
+		outString("3. Set timout per hop\n");
+		outString("4. Set node failure limit\n");
+		outString("5. Reset node ignore list\n");
 		outString("--> ");
 		u8_menuIn = inChar();
 		DELAY_MS(100);
@@ -241,16 +327,26 @@ int main(void) {
 		if (u8_menuIn == '1') {
 		}
 		else if (u8_menuIn == '2') {
-			outString("\nThis value is the ammount of time to wait during polling before skipping unresponsive node.\n");
-			outString("\nCurrent timeout (seconds): ");
-			outUint8Decimal(u8_pollingTimeout);
-			outString("\nNew timeout (seconds, max 15): ");
-			char timeout_buff[3];
-			inStringEcho(timeout_buff, 2);
-			outString("\nNew timeout value saved.");
+			outString("\nThis value is the maximum number of hops from the collector to the furthest node\n");
+			outString("\nCurrent number of hops: ");
+			outUint8Decimal(u8_numHops);
+			outString("\nNew number of hops: ");
+			char hops_buff[3];
+			inStringEcho(hops_buff, 2);
+			outString("\nNew number of hops saved.");
 			WAIT_UNTIL_TRANSMIT_COMPLETE_UART1();
 		}
 		else if (u8_menuIn == '3') {
+			outString("\nThis value is the timeout per network hop (default is 300ms).\n");
+			outString("\nCurrent timeout value: ");
+			outUint8Decimal(u32_hopTimeout);
+			outString("\nNew timeout value (milliseconds): ");
+			char timeout_buff[4];
+			inStringEcho(timeout_buff, 3);
+			outString("\nNew timeout value saved.");
+			WAIT_UNTIL_TRANSMIT_COMPLETE_UART1();
+		}
+		else if (u8_menuIn == '4') {
 			outString("\nThis value is the number of failures to respond a sensor node has before it will be ignored.\n");
 			outString("\nCurrent failure limit: ");
 			outUint8Decimal(u8_failureLimit);
@@ -275,7 +371,6 @@ int main(void) {
 		_INT1IE = 1;		//enable the interrupt
 
 		_LATB7 = 1;					//cut power to VDIP*/
-		_LATB7 = 0;
 
 		while (1) {
 			SLEEP();
